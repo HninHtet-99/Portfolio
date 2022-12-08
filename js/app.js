a6f1973574d78ac0112716a7f92e01affc5bdfc9
HninHtet-99 <hninhtet1113@gmail.com>
2022-12-08 15:26:46 +0630

    adding parallel img in projects section

--- js/app.js
+++ js/app.js
@@ -139,3 +139,14 @@ LettersRandomPlaces({
     random_scale: true,	
     rotate: -1
 })
+
+/* parallel */
+document.addEventListener('mousemove',parallax);
+function parallax(e){
+    document.querySelectorAll(".object").forEach(function(move){
+        let moving_value = move.getAttribute("data-value");
+        let x = (e.clientX * moving_value)/250;
+        let y = (e.clientY * moving_value/250);
+        move.style.transform = "translateX("+ x + "px) translateY(" + y + "px)";
+    })
+}
