cc6615943405cad7f8e63e102a689fa82b4d0d94
HninHtet-99 <hninhtet1113@gmail.com>
2022-12-15 23:23:34 +0630

    styling home text

--- js/app.js
+++ js/app.js
@@ -75,14 +75,14 @@ wow = new WOW(
   wow.init();
 
 /* rotate intro text */
-$('.intro-txt').on('mousemove',function(e){
-    centerX = $(this).width()/2;
-    let moveX = centerX - e.offsetX;
-    $(this).css({
-        'transform': "perspective(500px) rotateY("+moveX/30+"deg)"
-    })
+// $('.intro-txt').on('mousemove',function(e){
+//     centerX = $(this).width()/2;
+//     let moveX = centerX - e.offsetX;
+//     $(this).css({
+//         'transform': "perspective(500px) rotateY("+moveX/30+"deg)"
+//     })
     
-})
+// })
 /* svg tag cloud */
 var entries = [ 
    
