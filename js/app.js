b85afcf821d7919d44b4e4c5b187f8fc6cdb6b49
HninHtet-99 <hninhtet1113@gmail.com>
2022-12-16 19:20:24 +0630

    project section styling

--- js/app.js
+++ js/app.js
@@ -138,3 +138,65 @@ function parallax(e){
         move.style.transform = "translateX("+ x + "px) translateY(" + y + "px)";
     })
 }
+/* hover img in project section */
+const{ gsap } = window;
+        gsap
+        .timeline()
+        .set(".menu",{ autoAlpha: 1})
+        .from(".menu__item-innertext",{
+            delay: 1,
+            duration: 0.85,
+            xPercent: 25,
+            yPercent: 125,
+            stagger: 0.095,
+            skewY: gsap.utils.wrap([-8,8]),
+            ease: "expo.out"
+        })
+        .set(".menu",{
+            PointerEvent:"all"
+        });
+        gsap.defaults({
+            duration : 0.55,
+            ease: "expo.out",
+        });
+        const menuItems = document.querySelectorAll(".menu__item");
+        menuItems.forEach((item)=>{
+            const imgWrapper = item.querySelector(".menu__item-img_wrapper");
+            const imgWrapperBounds = imgWrapper.getBoundingClientRect();
+            let itemBounds = item.getBoundingClientRect;
+            const onMouseEnter = () =>{
+                gsap.set(imgWrapper, {
+                    scale: 0.8,
+                    xPercent: 25,
+                    yPercent: 50,
+                    rotation: -15,
+                })
+                gsap.to(imgWrapper, {opacity:1,scale:1,yPercent: 0,rotation: 0})
+            };
+            const onMouseLeave = ()=>{
+                gsap.to(imgWrapper,{
+                    opacity: 0,
+                    scale: 0.8,
+                    xPercent: 25,
+                    yPercent: -50,
+                    rotation: -15,
+                })
+            };
+            // const onMouseMove = ({x,y})=>{
+            //     let yOffset = itemBounds.top/imgWrapperBounds.height;
+            //     yOffset = gsap.utils.mapRange(0,1.5,-150,150,yOffset);
+            //     gsap.to(imgWrapper,{
+            //         duration: 1,
+            //         x: Math.abs(x-itemBounds.left)-imgWrapperBounds.width/1.55,
+            //         y: Math.abs(y-itemBounds.top)-imgWrapperBounds.height/2 - yOffset,
+
+
+            //     });
+            // };
+            item.addEventListener("mouseenter",onMouseEnter);
+            item.addEventListener("mouseleave",onMouseLeave);
+            // item.addEventListener("mousemove",onMouseMove);
+            window.addEventListener("resize",()=>{
+                itemBounds = item.getBoundingClientRect();
+            })
+        })
